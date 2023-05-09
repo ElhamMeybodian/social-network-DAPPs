86d8d96671aacd52aa34227fc1b4d797cb00b39c
Elham Mey <elhammey@gamil.com>
2023-05-09 11:02:00 +0330

    Initial project dapp Social Network

---
+++ src/contracts/SocialNetwork.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract SocialNetwork {
+    string public name;
+
+    constructor() public {
+        name = "DAPP Social Network";
+    }
+}
